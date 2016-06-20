cac68cb137d2f6c796ff98d05db6419381c28519
Halil Kaya <kayahalil@gmail.com>
2016-06-20 18:02:13 +0300

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='conj',
-      version='1.1',
+      version='1.2',
       description='Python conjugation module for Turkish',
       url='https://github.com/halilkaya/conj',
       author='Halil Kaya',

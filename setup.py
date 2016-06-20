a1c62179b5b764f569d04e0a44e7d9d2b351fc1c
Halil Kaya <kayahalil@gmail.com>
2016-06-20 17:47:34 +0300

    Minor version upgraded for 2.7 support to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='conj',
-      version='1.0',
+      version='1.1',
       description='Python conjugation module for Turkish',
       url='https://github.com/halilkaya/conj',
       author='Halil Kaya',

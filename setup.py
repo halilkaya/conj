5c0164fa978d724b6e916b003acb13f8beb6e345
Halil Kaya <kayahalil@gmail.com>
2016-06-23 22:47:30 +0300

    Version 1.4.1

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 from setuptools import setup
 
 setup(name='conj',
-      version='1.4',
+      version='1.4.1',
       description='Python conjugation module for Turkish',
       url='https://github.com/halilkaya/conj',
       author='Halil Kaya',
       author_email='halil@halilkaya.net',
       license='GPLv3',
       packages=['conj'],
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Natural Language :: Turkish',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+      ],
       scripts=['conj/__init__.py'],
       install_requires=['setuptools'],
       zip_safe=False)
